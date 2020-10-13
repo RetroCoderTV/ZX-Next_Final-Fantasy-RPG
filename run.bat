8a998b29e89a4208aa353dc78b8e984dac6150e9
RetroCoderTV <coderretro409@gmail.com>
2020-10-13 04:36:10 +0100

    tidied batch file

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 call build.bat
+
 if not errorlevel 1 (
 	C:\Dev\ZXNext\bin\hdfmonkey put C:\Dev\ZXNext\sdcard\cspect-next-2gb.img main.nex
 	C:\Dev\ZXNext\bin\CSpect.exe -r -brk -zxnext -nextrom -tv -basickeys -vsync -sound -mmc=C:\Dev\ZXNext\sdcard\cspect-next-2gb.img
